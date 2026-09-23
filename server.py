5da7cae742cd7926644e584277506a1bf09633fc
S.Sai Ram <162550829+Sai-Ram-Sagarla-23@users.noreply.github.com>
2026-09-23 19:15:44 +0530

    Change server host to 0.0.0.0 and use environment port

--- server.py
+++ server.py
@@ -132,4 +132,5 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
